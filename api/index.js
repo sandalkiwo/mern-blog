0f891ffb503ceb0bb98fe94e412fa76ceb571099
sandalkiwo <alimoktar00007@gmail.com>
2024-05-03 20:12:04 +0700

    Connect To The Databases

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB Is Connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(300, () => {
+app.listen(3000, () => {
   console.log("Server Is Running On Port 3000!");
 });
