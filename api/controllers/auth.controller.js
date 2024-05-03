bbfb9e5a6437af32314daeaeb3c68f610480361e
sandalkiwo <alimoktar00007@gmail.com>
2024-05-03 22:11:45 +0700

    add a middleware and a function to handler error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -12,10 +13,10 @@ export const signup = async (req, res) => {
     username === "" ||
     email === ""
   ) {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400, "All Fields Are Required"));
   }
 
-  const hashedPassword = bcryptjs.hashSync(password, 10)
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
     username,
@@ -27,6 +28,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json("User Created");
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
-
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -26,3 +25,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
