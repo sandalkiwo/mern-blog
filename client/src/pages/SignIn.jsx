62677cf6019d62a9ecaa0e424f69c6833f3bf041
sandalkiwo <alimoktar00007@gmail.com>
2024-05-04 04:54:08 +0700

    add and update reduc toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,90 +1,52 @@
-<<<<<<< HEAD
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
-import {  useDispatch, useSelector } from "react-redux";
-import { signinStart, signinSuccess, signinFailure } from "../redux/user/userSLice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signinStart,
+  signinSuccess,
+  signinFailure,
+} from "../redux/user/userSLice";
 
-const SignIn = () => {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
- const {loading, error: errorMessage} = useSelector((state) => state.user);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
-=======
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
-
-const SignIn = () => {
-  const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
-
->>>>>>> 375c2f635e4e54352732bd16836370aca1bdad57
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value.trim(),
-    });
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (!formData.email || !formData.password) {
-<<<<<<< HEAD
-      return dispatch(signinFailure("All fields are required"));
+      dispatch(signinFailure("Please fill all the fields"));
     }
-
     try {
       dispatch(signinStart());
-=======
-      return setErrorMessage("All fields are required");
-    }
-
-    try {
-      setLoading(true);
-      setErrorMessage(null);
->>>>>>> 375c2f635e4e54352732bd16836370aca1bdad57
       const res = await fetch("/api/auth/signin", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-
       if (data.success === false) {
-<<<<<<< HEAD
         dispatch(signinFailure(data.message));
       }
+
       if (res.ok) {
         dispatch(signinSuccess(data));
         navigate("/");
       }
     } catch (error) {
       dispatch(signinFailure(error.message));
-=======
-        return setErrorMessage(data.message);
-      }
-      setLoading(false);
-      if (res.ok) {
-        navigate("/");
-      }
-    } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
->>>>>>> 375c2f635e4e54352732bd16836370aca1bdad57
     }
   };
-
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        {/* Left */}
+        {/* left */}
         <div className="flex-1">
           <Link to={"/"} className=" text-4xl font-bold dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
@@ -101,27 +63,25 @@ const SignIn = () => {
             alkamal wujudkan impian anda!!!
           </p>
         </div>
-        {/* Rigth */}
+        {/* right */}
         <div className="flex-1">
-          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            <div className="">
-              <Label value="Your Email" />
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your email" />
               <TextInput
                 type="email"
-                placeholder="Email"
+                placeholder="name@company.com"
                 id="email"
                 onChange={handleChange}
-                required
               />
             </div>
-            <div className="">
-              <Label value="Your Password" />
+            <div>
+              <Label value="Your password" />
               <TextInput
                 type="password"
-                placeholder="***********"
+                placeholder="**********"
                 id="password"
                 onChange={handleChange}
-                required
               />
             </div>
             <Button
@@ -139,12 +99,8 @@ const SignIn = () => {
               )}
             </Button>
           </form>
-          <div className="flex text-sm mt-5 justify-between">
-<<<<<<< HEAD
-            <span>Dont have an account?</span>
-=======
-            <span>Don't have an account?</span>
->>>>>>> 375c2f635e4e54352732bd16836370aca1bdad57
+          <div className="flex gap-2 text-sm mt-5 justify-between">
+            <span>Dont Have an account?</span>
             <Link to="/sign-up" className="text-blue-500">
               Sign Up
             </Link>
@@ -158,6 +114,4 @@ const SignIn = () => {
       </div>
     </div>
   );
-};
-
-export default SignIn;
+}
