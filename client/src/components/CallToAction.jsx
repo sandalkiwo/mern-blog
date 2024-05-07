9eec1328b6b77d0f2ec0678a70280a223068c073
sandalkiwo <alimoktar00007@gmail.com>
2024-05-07 20:42:15 +0700

    add call to action to teh post page.

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,31 @@
+import { Button } from "flowbite-react";
+
+export default function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">
+          salah satu oraganisasi PMII pertami di kota rembang.{" "}
+        </h2>
+        <p className="text-gray-500 my-2">
+          CheckOut website partner kami!! dan program kerja kami!!!
+        </p>
+        <Button
+          gradientDuoTone="purpleToPink"
+          className="rounded-tl-xl rounded-bl-none"
+        >
+          <a
+            href="https://www.jurnaliska.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jurnaliska staika Rembang
+          </a>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjA8NWFDkJ_jSiAHdUQnxjTiVo1WrkIhbIkfHbbPRTrF8NPh8MJ6jjRldapUzIK8vAaI7tgPpPygystX1q4vu2v2cFQGC-4EL5ryNxW_K3UiC7u1sMUEVxg8MJpbuhM6QxD_sQ0teeUKK_u2kxsUiuE8sthyV41ueGH3OcgP3sC4VuQoRfZ0iUwz6aBCz0z/w1200-h630-p-k-no-nu/IMG-20231030-WA0012.jpg" />
+      </div>
+    </div>
+  );
+}
--- client/src/pages/PostPages.jsx
+++ client/src/pages/PostPages.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 export default function PostPages() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
@@ -62,6 +63,9 @@ export default function PostPages() {
       </div>
       <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{ __html: post && post.content }}>
          
+      </div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
       </div>
     </main>
   );
