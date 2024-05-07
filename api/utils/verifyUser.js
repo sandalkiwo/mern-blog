a54a442f05f6d7fc1d44b065d98bd4e85ffec7dd
dnkhrtlmgfrh <75060763+sandalkiwo@users.noreply.github.com>
2024-05-08 05:57:59 +0700

    Update verifyUser.js
    ganti enviremennya.

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -6,7 +6,7 @@ export const verifyToken = (req, res, next) => {
   if (!token) {
     return next(errorHandler(401, 'Unauthorized'));
   }
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET_COOKIE, (err, user) => {
     if (err) {
       return next(errorHandler(401, 'Unauthorized'));
     }
