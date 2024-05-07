4673c9cb553a6a8c10cb84d18f8362c116d7b1f6
sandalkiwo <alimoktar00007@gmail.com>
2024-05-08 04:45:06 +0700

    add edit funcionality to the comment from component.

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { createComment, getPostComments, likeComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  editComment,
+  getPostComments,
+  likeComment,
+} from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
 router.get("/getpostcomments/:postId", getPostComments);
-router.put("/likeComent/:commentId", verifyToken, likeComment)
+router.put("/likeComent/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
 
 export default router;
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -24,6 +24,31 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
     getUser();
   }, [comment]);
 
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditedContent(comment.content);
+  };
+
+  const handleSave = async () => {
+    try {
+      const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: editedContent,
+        }),
+      });
+      if(res.ok) {
+        setIsEditing(false);
+        onEdit(comment, editedContent)
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
       <div className="flex-shrink-0 mr-3">
@@ -54,6 +79,7 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
                 type="button"
                 size="sm"
                 gradientDuoTone="purpleToBlue"
+                onClick={handleSave}
               >
                 Save
               </Button>
@@ -76,16 +102,29 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
                 type="button"
                 onClick={() => onLike(comment._id)}
                 className={`text-gray-400 hover:text-blue-500 ${
-                  currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'
+                  currentUser &&
+                  comment.likes.includes(currentUser._id) &&
+                  "!text-blue-500"
                 }`}
               >
                 <FaThumbsUp className="text-sm" />
               </button>
               <p className="text-gray-400">
-                {
-                  comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")
-                }
+                {comment.numberOfLikes > 0 &&
+                  comment.numberOfLikes +
+                    " " +
+                    (comment.numberOfLikes === 1 ? "like" : "likes")}
               </p>
+              {currentUser &&
+                (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                  <button
+                    type="button"
+                    onClick={handleEdit}
+                    className="text-gray-400 hover:text-blue-500"
+                  >
+                    Edit
+                  </button>
+                )}
             </div>
           </>
         )}
