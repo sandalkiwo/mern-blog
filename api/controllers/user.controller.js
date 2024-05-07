b86836911f91a55f84628f76afb492f7d27daaa8
sandalkiwo <alimoktar00007@gmail.com>
2024-05-07 14:39:53 +0700

    create getusers api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,6 +6,50 @@ export const test = (req, res) => {
   res.json({ message: "API Is Working!!!" });
 };
 
+// Get All Users
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to get all users"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const userWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: userWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 // Edit akun Pengguna
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
@@ -70,23 +114,11 @@ export const deleteUser = async (req, res, next) => {
 
 export const signout = (req, res, next) => {
   try {
-    res.clearCookie("access_token").status(200).json("User has been signed out");
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json("User has been signed out");
   } catch (error) {
     next(error);
   }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
