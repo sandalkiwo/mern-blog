40a4d2b7702f409445ab5f3a47abdeaa66005beb
sandalkiwo <alimoktar00007@gmail.com>
2024-05-12 11:45:13 +0700

    complete the home page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/post/getPosts");
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div>
-      
+      <div className="flex flex-col gap-6 lg:p-20 p-3 max-w-6xl mx-auto">
+        <h1 className="text-2xl font-bold lg:text-6xl">Welcame To My Blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View All Post
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-2">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              className="text-lg text-teal-500 hover:underline text-center"
+              to={"/search"}
+            >
+              Views All Posts
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
