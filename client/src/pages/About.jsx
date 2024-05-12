7cd01b9796ea0d8364f2b787f6256722402559b3
sandalkiwo <alimoktar00007@gmail.com>
2024-05-12 18:51:30 +0700

    complete about and projects pages.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="min -h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Dian'Blogs
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              aliquam facilis error explicabo inventore ullam beatae eveniet
+              illum quisquam nemo, enim assumenda quibusdam atque, at, veritatis
+              vitae molestias ut quis totam maxime cum consequatur sapiente
+              veniam eos! Ad voluptatibus odio iste, tenetur non dignissimos
+              atque, inventore ea molestiae numquam laborum quisquam officia
+              sint odit distinctio aperiam similique amet architecto suscipit
+              soluta, laboriosam ipsa optio animi eveniet? Consequuntur earum
+              nam alias.
+            </p>
 
-export default About
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              aliquam facilis error explicabo inventore ullam beatae eveniet
+              illum quisquam nemo, enim assumenda quibusdam atque, at, veritatis
+              vitae molestias ut quis totam maxime cum consequatur sapiente
+              veniam eos! Ad voluptatibus odio iste, tenetur non dignissimos
+              atque, inventore ea molestiae numquam laborum quisquam officia
+              sint odit distinctio aperiam similique amet architecto suscipit
+              soluta, laboriosam ipsa optio animi eveniet? Consequuntur earum
+              nam alias.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est
+              aliquam facilis error explicabo inventore ullam beatae eveniet
+              illum quisquam nemo, enim assumenda quibusdam atque, at, veritatis
+              vitae molestias ut quis totam maxime cum consequatur sapiente
+              veniam eos! Ad voluptatibus odio iste, tenetur non dignissimos
+              atque, inventore ea molestiae numquam laborum quisquam officia
+              sint odit distinctio aperiam similique amet architecto suscipit
+              soluta, laboriosam ipsa optio animi eveniet? Consequuntur earum
+              nam alias.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import CallToAction from "../components/CallToAction";
 
 const Projects = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen max-w-2xl mx-auto items-center justify-center flex flex-col p-3 gap-6">
+      <h1 className="text-3xl font-semibold">Project's</h1>
+      <p className="text-md text-gray-500 ">
+        Blog tentang sbuah pergerakan diman pergerakan ini membutuhkan suatu
+        perubahan yang signifikan bigi masayarakt sekitar.
+      </p>
+      <CallToAction />
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
